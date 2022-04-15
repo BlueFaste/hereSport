2e88172f2da733d462931ff2e8debfeeed3447bb
BlueFaste <fassot.caroline@gmail.com>
2022-04-15 11:39:42 +0200

    Create Header and Button components

---
+++ components/common/Button.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="classes">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    background: {
+      type: String,
+      default: "bg-gray-400",
+    },
+    background_hover: {
+      type: String,
+      default: "bg-gray-500",
+    },
+    with: {
+      type: String,
+      default: "w-full",
+    },
+    margin: {
+      type: String,
+      default: "mx-2 my-1",
+    },
+    padding: {
+      type: String,
+      default: "px-3 py-1",
+    },
+    rounded: {
+      type: String,
+      default: null,
+    },
+  },
+
+  computed: {
+    classes() {
+      return `
+      ${this.background}
+      hover:${this.background_hover}
+      ${this.with}
+      ${this.margin}
+      ${this.padding}
+      ${this.rounded ? this.rounded : ""}`;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ components/common/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header
+    class="bg-gradient-to-r from-green-300 to-green-500 flex justify-between px-44 py-5 items-center"
+  >
+    <h1>HereSport</h1>
+    <nav>
+      <ul class="flex justify-between">
+        <li v-for="item in nav" :key="item.name">
+          <NuxtLink :to="item.link">{{ item.name }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "header",
+
+  data() {
+    return {
+      nav: [
+        {
+          name: "Element 1",
+          link: "/",
+        },
+        {
+          name: "Element 2",
+          link: "/test/",
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style></style>
